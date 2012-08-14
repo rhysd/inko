001e25df8b59d373a25f25f441ff1493eb524dc9
rhysd <lin90162@yahoo.co.jp>
2012-08-15 02:02:18 +0900

    indent 2 -> 4.

--- lib/inko.rb
+++ lib/inko.rb
@@ -8,41 +8,45 @@ require 'shellwords'
 
 module Inko extend self
 
-    def filter? status
-        !status.user || !status.text
+  def filter? status
+    !status.user || !status.text
+  end
+
+  # modify screen_name and text to make them easy to pronounce
+  # _,!
+  # cf http://d.hatena.ne.jp/nacookan/20081220/1229745342
+  def pronunciationize status
+    status.user.screen_name = status.user.screen_name.gsub(/_/, ' ')
+                                                     .gsub(/([a-zA-Z]+|[0-9]+)/){$1 + ' '}
+                                                     .gsub(/@/,'@. ') 
+                                                     .gsub(/！/,'!')
+                                                     .gsub(/ｗ+/, ' 笑い ')
+    status.text = Shellwords::escape status.text.split(/\n/).join(" ")
+    status
+  end
+
+  def run
+    begin
+      UserStream.configure do |config|
+        config.consumer_key       = Config::ConsumerKey
+        config.consumer_secret    = Config::ConsumerSecret
+        config.oauth_token        = Config::OAuthToken
+        config.oauth_token_secret = Config::OAuthSecret
+      end
+    rescue UserStream::Unauthorized => e
+      STDERR.puts "contents of #{Config::ConfigFileName} is invalid!"
+      raise e
     end
 
-    # modify screen_name and text to make them easy to pronounce
-    # _,!
-    # cf http://d.hatena.ne.jp/nacookan/20081220/1229745342
-    def pronunciationize status
-        status.user.screen_name = status.user.screen_name.gsub(/_/, ' ').gsub(/([a-zA-Z]+|[0-9]+)/){$1 + ' '}.gsub(/@/,'@. ') 
-        status.text = Shellwords::escape status.text.split(/\n/).join(" ")
-        status
+    UserStream::client.user do |status|
+      unless filter? status
+        status = pronunciationize status
+        # puts "#{status.user.screen_name}: "
+        # puts status.text
+        `say -v #{Config::VoiceActor} #{status.user.screen_name} さんのツイート}`
+        `say -v #{Config::VoiceActor} #{status.text}`
+      end
     end
-
-    def run
-        begin
-            UserStream.configure do |config|
-                config.consumer_key       = Config::ConsumerKey
-                config.consumer_secret    = Config::ConsumerSecret
-                config.oauth_token        = Config::OAuthToken
-                config.oauth_token_secret = Config::OAuthSecret
-            end
-        rescue UserStream::Unauthorized => e
-            STDERR.puts "contents of #{Config::ConfigFileName} is invalid!"
-            raise e
-        end
-
-        UserStream::client.user do |status|
-            unless filter? status
-                status = pronunciationize status
-                # puts "#{status.user.screen_name}: "
-                # puts status.text
-                `say -v #{Config::VoiceActor} #{status.user.screen_name} さんのツイート}`
-                `say -v #{Config::VoiceActor} #{status.text}`
-            end
-        end
-    end
+  end
 
 end
--- lib/inko/config.rb
+++ lib/inko/config.rb
@@ -3,37 +3,39 @@
 require 'yaml'
 
 module Inko
-    module Config
-        # file name for credential information
-        ConfigFileName = File.expand_path('~') + '/.inkorc.yml'
+  module Config
+    # file name for credential information
+    ConfigFileName = File.expand_path('~') + '/.inkorc.yml'
 
-        # if file is not found
-        unless File.exist? ConfigFileName
-            File.open ConfigFileName,"w" do |file|
-                file.print <<-EOS.gsub(/^\s+/, '')
-                        # Choose voice actor from System Setting.
-                        # To call off in Japanese, download voice data (Kyoko).
-                        voice_actor:        Alex
+    # if file is not found
+    unless File.exist? ConfigFileName
+      File.open ConfigFileName,"w" do |file|
+        file.print <<-EOS.gsub(/^\s+/, '')
+          # Choose voice actor from System Setting.
+          # To call off in Japanese, download voice data (Kyoko).
+          voice_actor:        Alex
 
-                        # Get app keys at https://dev.twitter.com/ and write them.
-                        consumer_key:       YourConsumerKey
-                        consumer_secret:    YourConsumerSecretKey
-                        oauth_token:        YourOAuthToken
-                        oauth_token_secret: YourOAuthSecretToken
-                EOS
 
-                STDERR.puts "Configuration-keys are not found."
-                STDERR.puts "Write your consumer keys and OAuth keys to #{ConfigFileName}"
-            end
-            system 'bash', '-c', (ENV['EDITOR'] || 'vi')+' "$@"', '--', ConfigFileName
-        end
 
-        # load configuration from file
-        yaml = YAML.load(File.open(Config::ConfigFileName).read)
-        VoiceActor     = yaml['voice_actor']
-        ConsumerKey    = yaml['consumer_key']
-        ConsumerSecret = yaml['consumer_secret']
-        OAuthToken     = yaml['oauth_token']
-        OAuthSecret    = yaml['oauth_token_secret']
+          # Get app keys at https://dev.twitter.com/ and write them.
+          consumer_key:       YourConsumerKey
+          consumer_secret:    YourConsumerSecretKey
+          oauth_token:        YourOAuthToken
+          oauth_token_secret: YourOAuthSecretToken
+        EOS
+
+        STDERR.puts "Configuration-keys are not found."
+        STDERR.puts "Write your consumer keys and OAuth keys to #{ConfigFileName}"
+      end
+      system 'bash', '-c', (ENV['EDITOR'] || 'vi')+' "$@"', '--', ConfigFileName
     end
+
+    # load configuration from file
+    yaml = YAML.load(File.open(Config::ConfigFileName).read)
+    VoiceActor     = yaml['voice_actor']
+    ConsumerKey    = yaml['consumer_key']
+    ConsumerSecret = yaml['consumer_secret']
+    OAuthToken     = yaml['oauth_token']
+    OAuthSecret    = yaml['oauth_token_secret']
+  end
 end
